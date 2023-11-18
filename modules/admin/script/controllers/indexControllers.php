246875cdd58f9ba9cb259fec72800d32c2f90abb
Aanhiu <buianh2592003@gamil.com>
2023-11-18 17:03:44 +0700

    du_an1 project

---
+++ modules/admin/script/controllers/indexControllers.php
@@ -0,0 +1,16 @@
+<?php
+
+// hàm xử lí ảnh khi ảnh đã tải lên 
+// cần xử lí thêm
+function construct(){
+    // hàm khởi tạo 
+
+}
+
+function uploadXulianh(){
+    var_dump($_FILES);
+    
+}
+
+
+?>
